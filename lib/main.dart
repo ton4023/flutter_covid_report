b919b4df2b9b1a639fe871b54c9b7a50c2dca224
ton4023 <ton4023@gmail.com>
2021-09-13 11:05:59 +0700

    add src

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class Init {
     // This is where you can initialize the resources needed by your app while
     // the splash screen is displayed.  Remove the following example because
     // delaying the user experience is a bad design practice!
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 10));
   }
 }
 
